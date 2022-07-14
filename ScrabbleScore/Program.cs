691eeb15b21732f731d4411ec7defabddf89fdbd
zbl14 <ifthereisoneday@gmail.com>
2022-07-14 12:03:04 -0700

    add ui for user input a word return the score

--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -7,7 +7,24 @@ namespace ScrabbleScore
   {
    public static void Main() 
    {
-
+    Console.WriteLine("Please enter a word to see its scrabble score");
+    string word = Console.ReadLine();
+    int score = Scrabble.Score(word);
+    switch (score)
+    {
+      case 0:
+        Main();
+        break;
+      default:
+        Console.WriteLine("The scrabble score for " + word + " is " + score);
+        Console.WriteLine("Would you like to see another word? Y for yes, enter for quit");
+        string response = Console.ReadLine();
+        if (response == "Y" || response == "y")
+        {
+          Main();
+        }
+        break;
+    } 
    }
   }
 }
